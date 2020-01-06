a6be68e6a4765498b6c30b810506178a40c2f87c
Tomer Zager <tomerzager@gmail.com>
2020-01-06 11:23:42 +0200

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,63 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.AtomTalon;
+
+public class Drivetrain extends SubsystemBase {
+  
+  // FAKE PIDF VALUES!
+  public AtomTalon leftMaster = new AtomTalon(1, 0, 1, 0.1, 0, 1, 0);
+  public AtomTalon rightMaster = new AtomTalon(2, 0, 1, 0.1, 0, 1, 0);
+
+  private WPI_VictorSPX m_leftSlave = new WPI_VictorSPX(3);
+  private WPI_VictorSPX m_rightSlave = new WPI_VictorSPX(4);
+
+  private DifferentialDrive m_drive = new DifferentialDrive(leftMaster, rightMaster);
+
+  /**
+   * Creates a new Drivetrain.
+   */
+  public Drivetrain() {
+    m_leftSlave.configFactoryDefault();
+    m_rightSlave.configFactoryDefault();
+
+    m_leftSlave.follow(leftMaster);
+    m_rightSlave.follow(leftMaster);
+
+    leftMaster.setInverted(false);
+    rightMaster.setInverted(true);
+
+    m_leftSlave.setInverted(InvertType.FollowMaster);
+    m_rightSlave.setInverted(InvertType.FollowMaster);
+
+    leftMaster.configEncoder();
+    rightMaster.configEncoder();
+  }
+
+  public void arcade(double y, double x) {
+    m_drive.arcadeDrive(y, x);
+  }
+
+  /**
+   * Test method for auto command.
+   */
+  public void testAutoCommand() {
+    System.out.println("JUST A TEST, RELAX");
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
---
+++ src/main/java/frc/robot/util/AtomTalon.java
@@ -0,0 +1,63 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.util;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import frc.robot.Constants;
+
+/**
+ * Customized, better Talon SRX.
+ */
+public class AtomTalon extends WPI_TalonSRX {
+
+    private int m_PID;
+    private int m_Slot;
+    private double m_distancePerPulse;
+
+    public AtomTalon(int port, int pidIdx, int slotIdx, double kP, double kI, double kD, double kF) {
+        super(port);
+        super.configFactoryDefault();
+
+        this.m_PID = pidIdx;
+        this.m_Slot = slotIdx;
+
+        super.config_kP(m_Slot, kP);
+        super.config_kI(m_Slot, kI);
+        super.config_kD(m_Slot, kD);
+        super.config_kF(m_Slot, kF);
+    }
+
+    public void configEncoder() {
+        super.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, m_PID, 10);
+        this.reset();
+        this.m_distancePerPulse = Math.PI * Constants.kWheelDiameterInches / Constants.kUPR;
+    }
+
+    public void reset() {
+        super.setSelectedSensorPosition(0);
+    }
+
+    public double unitsToInches(int units) {
+        return units * this.m_distancePerPulse;
+    }
+
+    public int inchesToUnits(double inches) {
+        return (int) (inches / this.m_distancePerPulse);
+    }
+    
+    public double getDistanceInches() {
+        return super.getSelectedSensorPosition() * this.m_distancePerPulse;
+    }
+
+    public double getVelocityInches() {
+        return super.getSelectedSensorVelocity() * this.m_distancePerPulse * 10.0;
+    }
+
+}
