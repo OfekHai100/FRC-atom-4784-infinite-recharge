23efae6b065d1e72256ca3f8ec5c9812e82fccc0
Tomer Zager <tomerzager@gmail.com>
2020-03-30 20:49:16 +0300

    Minor change

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import frc.robot.subsystems.Roller;
 import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Shooter.Position;
 
 public class IntakeCommand extends CommandBase {
   
