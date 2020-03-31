1664f2d347ccc25f0939310e1795ace488ff125a
Tomer Zager <tomerzager@gmail.com>
2020-03-31 14:41:42 +0300

    Minor update in PathManager.java

--- src/main/java/frc/robot/pathing/PathManager.java
+++ src/main/java/frc/robot/pathing/PathManager.java
@@ -92,7 +92,6 @@ public class PathManager {
      * @return Starting position, as a {@link Pose2d}.
      */
     public static Pose2d getStartingPosition(Path path) {
-
         switch(path) {
             case RED_PORT:
                 return new Pose2d(new Translation2d(3.019, -2.453), new Rotation2d(-1, 0));
@@ -108,10 +107,8 @@ public class PathManager {
                 return new Pose2d(new Translation2d(13.013, -2.56), new Rotation2d(1, 0));
             default:
                 // Can not reach here!
+                return null;
         }
-
-        // Can not reach here!
-        return null;
     }
 
 }
