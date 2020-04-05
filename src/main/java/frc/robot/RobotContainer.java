7fb47ffaf9a6d4f6e387d616c098b153f1bdc4f6
Tomer Zager <tomerzager@gmail.com>
2020-04-05 20:50:03 +0300

    Minor update

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,8 +37,6 @@ import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Shooter.Position;
 import frc.robot.util.CommandGenerator;
 import frc.robot.util.PSController;
-import frc.robot.vision.Calculation;
-import frc.robot.vision.VisionController;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
