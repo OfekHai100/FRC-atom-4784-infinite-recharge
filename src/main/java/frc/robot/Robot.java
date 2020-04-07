bea97ed3805218dd5ce1baa0d4b69e58381a5b1e
Tomer Zager <tomerzager@gmail.com>
2020-04-08 01:16:21 +0300

    Added Shuffleboard support

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -34,12 +34,11 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    Dashboard.putChooser();
+    Dashboard.setup();
     m_led = LED.getInstance();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    Dashboard.putMatchData();
   }
 
   /**
@@ -51,6 +50,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
+    Dashboard.loop();
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
--- src/main/java/frc/robot/util/Dashboard.java
+++ src/main/java/frc/robot/util/Dashboard.java
@@ -7,12 +7,22 @@
 
 package frc.robot.util;
 
+import java.util.Map;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import frc.robot.pathing.PathManager.Path;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.LED;
+import frc.robot.subsystems.Roller;
+import frc.robot.subsystems.Shooter;
 
 /**
  * Dashboard++.
@@ -22,55 +32,89 @@ public class Dashboard {
     // Path chooser:
     private static SendableChooser<Path> m_chooser = new SendableChooser<Path>();
 
+    private static DriverStation m_DS;
+
+    private static ShuffleboardTab m_setupTab = Shuffleboard.getTab("Setup Tab");
+    private static ShuffleboardTab m_autoTab = Shuffleboard.getTab("Autonomous Tab");
+    private static ShuffleboardTab m_teleopTab = Shuffleboard.getTab("Teleop Tab");
+
+    private static NetworkTableEntry m_heading, m_distance, m_angle;
+    private static NetworkTableEntry m_reverseClimb, m_reverseIntake, m_activateIntake, m_position, m_vision;
+
+    private static boolean m_passedAuto;
+    private static boolean m_passedTeleop;
+
     /**
      * Utility class, private constructor.
      */
     private Dashboard() {
     }
 
+    public static void setup() {
+        m_DS = DriverStation.getInstance();
+
+        putChooserData();
+        putMatchData();
+        putAutonomousData();
+        putTeleopData();
+        Shuffleboard.selectTab("Setup Tab");
+
+        m_passedAuto = false;
+        m_passedTeleop = false;
+    }
+
+    public static void loop() {
+        if(m_DS.isAutonomous()) {
+            if(!m_passedAuto) {
+                Shuffleboard.selectTab("Autonomous Tab");
+                m_passedAuto = true;
+            }
+            updateAutonomousData();
+        } else if(m_DS.isOperatorControl()) {
+            if(!m_passedTeleop) {
+                Shuffleboard.selectTab("Teleop Tab");
+                m_passedTeleop = true;
+            }
+            updateTeleopData();
+        }
+    }
+
     /**
      * Displays the {@link Path} chooser at the begining of every match.
      */
-    public static void putChooser() {
-        SmartDashboard.putData(m_chooser);
+    private static void putChooserData() {
         m_chooser.addOption("Red Port", Path.RED_PORT);
         m_chooser.addOption("Red Middle", Path.RED_MIDDLE);
         m_chooser.addOption("Red Blue-Feeder", Path.RED_BLUE_FEEDER);
         m_chooser.addOption("Blue Port", Path.BLUE_PORT);
         m_chooser.addOption("Blue Middle", Path.BLUE_MIDDLE);
         m_chooser.addOption("Blue Red-Feeder", Path.BLUE_RED_FEEDER);
-    }
 
-    /**
-     * Returns the selected path for autonomous routine.
-     * @return Selected path.
-     */
-    public static Path getSelectedPath() {
-        return m_chooser.getSelected();
+        m_setupTab.add("Routine chooser", m_chooser);
     }
 
     /**
      * Displays match data every match for the drivers.
      */
-    public static void putMatchData() {
-        String alliance;
+    private static void putMatchData() {
+        boolean isRed;
         String matchType;
         String event;
         int matchNum;
         int station;
         
-        if(DriverStation.getInstance().isFMSAttached()) {
+        if(m_DS.isFMSAttached()) {
             matchNum = DriverStation.getInstance().getMatchNumber();
             event = DriverStation.getInstance().getEventName();
             station = DriverStation.getInstance().getLocation();
             
-            if(DriverStation.getInstance().getAlliance() == Alliance.Blue) {
-                alliance = "Blue Alliance";
+            if(m_DS.getAlliance() == Alliance.Blue) {
+                isRed = false;
             } else {
-                alliance = "Red Alliance";
+                isRed = true;
             }
 
-            switch(DriverStation.getInstance().getMatchType()) {
+            switch(m_DS.getMatchType()) {
                 case Qualification:
                     matchType = "QUAL";
                 case Elimination:
@@ -81,36 +125,125 @@ public class Dashboard {
         } else {
             matchType = "PRCT";
             matchNum = 0;
-            alliance = "Red Alliance";
+            isRed = true;
             station = 0;
             event = "Virtual Practice Event";
         }
 
-        SmartDashboard.putNumber(matchType, matchNum);
-        SmartDashboard.putString(alliance, "Station " + station);
-        SmartDashboard.putString("Competing at:", event);
+        m_setupTab.add("Alliance", isRed)
+                  .withProperties(Map.of("Color when true", "#FF0000", "Color when false", "#FF5555"))
+                  .withSize(6, 3)
+                  .withPosition(0, 0)
+                  .withWidget(BuiltInWidgets.kBooleanBox);
+
+        m_setupTab.add("Match", matchType + " " + matchNum)
+                  .withSize(6, 1)
+                  .withPosition(0, 4)
+                  .withWidget(BuiltInWidgets.kTextView);
+        
+        m_setupTab.add("Event", "Competing at " + event + ", Station number " + station)
+                  .withSize(6,1)
+                  .withPosition(0, 5)
+                  .withWidget(BuiltInWidgets.kTextView);
     }
 
     /**
-     * Checks the color for position control (Shield Generator phase 3 only!), return the required color
-     * for the ColorSensorV3 which is used for the Control Panel tasks.
-     * @return The required game color for the sensor.
+     * Displays and initializes all Autonomous Tab values.
      */
-    public static String getGameColor() {
-        String gameData = DriverStation.getInstance().getGameSpecificMessage();
-        if(gameData.length() > 0) {
-            switch(gameData.charAt(0)) {
-                case 'B':
-                    return "Red";
-                case 'G':
-                    return "Yellow";
-                case 'R':
-                    return "Blue";
-                case 'Y':
-                    return "Green";
-            }
-        }
-        return null;
+    private static void putAutonomousData() {    
+        m_heading = m_autoTab.add("Robot Heading", 0.0)
+                             .withSize(4, 8)
+                             .withPosition(0, 0)
+                             .withWidget(BuiltInWidgets.kTextView)
+                             .getEntry();
+
+        m_distance = m_autoTab.add("Robot Distance", 0.0)
+                              .withSize(4, 8)
+                              .withPosition(0, 5)
+                              .withWidget(BuiltInWidgets.kTextView)
+                              .getEntry();
+
+        m_angle = m_autoTab.add("Shooter Angle", 0.0)
+                           .withSize(4, 8)
+                           .withPosition(0, 9)
+                           .withWidget(BuiltInWidgets.kTextView)
+                           .getEntry();
+    }
+
+    /**
+     * Updates the Autonomous Tab values periodically.
+     */
+    private static void updateAutonomousData() {
+        Drivetrain drive = Drivetrain.getInstance();
+        Shooter shooter = Shooter.getInstance();
+
+        m_heading.setDouble(drive.getHeading());
+        m_distance.setDouble(drive.getDistance());
+        m_angle.setDouble(shooter.getAngle());
+    }
+    
+    /**
+     * Displays and initializes all Teleop Tab values.
+     */
+    private static void putTeleopData() {
+        m_reverseClimb = m_teleopTab.add("Climber - Reversed", false)
+                                    .withProperties(Map.of("Color when true", "#008000", "Color when false", "#FF0000"))
+                                    .withSize(4, 4)
+                                    .withPosition(0, 1)
+                                    .withWidget(BuiltInWidgets.kBooleanBox)
+                                    .getEntry();
+
+        m_reverseIntake = m_teleopTab.add("Intake - Reversed", false)
+                                     .withProperties(Map.of("Color when true", "#008000", "Color when false", "#FF0000"))
+                                     .withSize(4, 4)
+                                     .withPosition(6, 1)
+                                     .withWidget(BuiltInWidgets.kBooleanBox)
+                                     .getEntry();
+
+        m_activateIntake = m_teleopTab.add("Intake - Activated", false)
+                                      .withProperties(Map.of("Color when true", "#008000", "Color when false", "#FF0000"))
+                                      .withSize(4, 4)
+                                      .withPosition(6, 6)
+                                      .withWidget(BuiltInWidgets.kBooleanBox)
+                                      .getEntry();
+
+        m_vision = m_teleopTab.add("Vision Mode", false)
+                              .withProperties(Map.of("Color when true", "#008000", "Color when false", "#FF0000"))
+                              .withSize(4, 4)
+                              .withPosition(12, 1)
+                              .withWidget(BuiltInWidgets.kBooleanBox)
+                              .getEntry();
+
+        m_position = m_teleopTab.add("Shooter - Position", "")
+                                .withSize(8, 4)
+                                .withPosition(14, 1)
+                                .withWidget(BuiltInWidgets.kTextView)
+                                .getEntry();
+    }
+
+    /**
+     * Updates the Teleop Tab values periodically.
+     */
+    private static void updateTeleopData() {
+        Climber climber = Climber.getInstance();
+        LED led = LED.getInstance();
+        Roller roller = Roller.getInstance();
+        Shooter shooter = Shooter.getInstance();
+
+
+        m_reverseClimb.setBoolean(climber.getReverse());
+        m_reverseIntake.setBoolean(roller.getReverse());
+        m_activateIntake.setBoolean(roller.isIntakeModeActivated());
+        m_vision.setBoolean(led.isVisionActivated());
+        m_position.setString(shooter.getPositionAsString());
+    }
+
+    /**
+     * Returns the selected path for autonomous routine.
+     * @return Selected path.
+     */
+    public static Path getSelectedPath() {
+        return m_chooser.getSelected();
     }
 
 }
