7324e02a056a990d7537c14b76943274ace5c13f
Tomer Zager <tomerzager@gmail.com>
2020-06-22 23:40:56 +0300

    Fix on VisionController.java

--- src/main/java/frc/robot/vision/VisionController.java
+++ src/main/java/frc/robot/vision/VisionController.java
@@ -206,7 +206,7 @@ public class VisionController {
     private double calculateAndCheckAngle(double currentAngle) {
         double calculated = calculateAngle();
         if(inRange(currentAngle, calculated)) {
-            return currentAngle;
+            return 0.0;
         }
         if(checkAngle(calculated)) {
             return calculated;
